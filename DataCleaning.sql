c3bda263095226c8130409630214b9077e4a1cce
USERISM <88325167+USERISM@users.noreply.github.com>
2026-02-27 04:27:30 +0100

    fin

--- DataCleaning.sql
+++ DataCleaning.sql
@@ -125,3 +125,52 @@ set `date` = str_to_date(`date`, '%m/%d/%Y');
 
 alter table layoffs_staging2
 modify column `date` date;
+
+
+-- Null or blank values
+
+select *
+from layoffs_staging2
+where total_laid_off is null;
+
+select *
+from layoffs_staging2
+where industry is null
+or industry = '';
+
+select t1.industry, t2.industry
+from layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company = t2.company
+    and t1.location = t2.location
+    where t1.industry is null 
+    and t2.industry is not null;
+    
+update layoffs_staging2
+set industry = null
+where industry='';    
+    
+update layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company = t2.company
+set t1.industry = t2.industry
+where t1.industry is null 
+and t2.industry is not null;
+
+-- Remove any columns
+
+select *
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+delete
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+select *
+from layoffs_staging2;
+
+alter table layoffs_staging2
+drop column row_num;
